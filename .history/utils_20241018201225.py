9edfb00c29d4ef967337db35aba1857ba2c7119e
NguyenTien204 <vantiennguyen1424@gmail.com>
2024-10-18 20:34:40 +0700

    toi uu big data cho causal model

--- .history/utils_20241018201225.py
+++ .history/utils_20241018201225.py
@@ -12,21 +12,12 @@ if not os.path.exists(data_dir):
 
 # Hàm kiểm tra file có tồn tại không
 def check_file_exists(filepath):
-    return os.path.isfile(filepath)\
+    return os.path.isfile(filepath)
 
-# Hàm lưu model 
 def save_model(model, filename='climate_change_impact_model.pkl'):
     joblib.dump(model, filename)
     print(f"Model saved to {filename}")
 
-#Hàm load model
-def load_model(filename):
-    try:
-        model = joblib.load(filename)
-        print(f"Model loaded from {filename}")
-        return model
-    except FileNotFoundError:
-        print(f"Model file {filename} not found. Please train the model first.")
-        return None
+
 
 #------------------------------------------------------------------------------------------------------------------------------------>
